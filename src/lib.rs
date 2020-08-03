478a3aa5abe3173e3172b09193cbe3f037c82731
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-08-03 20:08:51 +0200

    Added the first version

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+//! Implements the `print!`, `println!` and `dbg!` macros so they can be used in the kernel without the use of an allocator.
+//!
+//! ## Usage
+//!
+//! Exactly as you'd use the original macros from the standard library.
+//!
+//! ## Features
+//!
+//! - `std_name`: Allows you to use the macros without the `kernel_` prefix.
+
+#![no_std]
+
+#[doc(hidden)]
+pub mod logger;
+
+#[cfg(feature = "std_name")]
+#[doc(hidden)]
+pub mod std_name {
+    pub use super::kernel_dbg as dbg;
+    pub use super::kernel_print as print;
+    pub use super::kernel_println as println;
+}
+#[cfg(feature = "std_name")]
+pub use std_name::*;
+
+/// Macro for printing the value of a given expression for quick and dirty debugging.
+///
+/// Does not panic on failure to write - instead silently ignores errors.
+///
+/// See [`dbg!`](https://doc.rust-lang.org/std/macro.dbg.html) for full documentation.
+#[macro_export]
+macro_rules! kernel_dbg {
+    () => {
+        $crate::println!("[{}:{}]", file!(), line!());
+    };
+    ($val:expr) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::println!("[{}:{}] {} = {:#?}",
+                    file!(), line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    // Trailing comma with single argument is ignored
+    ($val:expr,) => { $crate::dbg!($val) };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
+/// Prints to the standard output.
+///
+/// Does not panic on failure to write - instead silently ignores errors.
+///
+/// See [`print!`](https://doc.rust-lang.org/std/macro.print.html) for full documentation.
+#[macro_export]
+macro_rules! kernel_print {
+    ($($arg:tt)*) => {
+    #[allow(unused_must_use)]
+    {
+        let mut logger = $crate::Logger::new();
+        logger.write_fmt(format_args!($($arg)*));
+    }
+    };
+}
+
+/// Prints to the standard output, with a newline.
+///
+/// Does not panic on failure to write - instead silently ignores errors.
+///
+/// See [`println!`](https://doc.rust-lang.org/std/macro.println.html) for full documentation.
+#[macro_export]
+macro_rules! kernel_println {
+    () => { $crate::kernel_println!("") };
+    ($($arg:tt)*) => {
+        #[allow(unused_must_use)]
+        {
+            let mut logger = $crate::Logger::new();
+            logger.write_fmt(format_args!($($arg)*));
+            logger.write_nl();
+        }
+    };
+}
---
+++ src/logger.rs
@@ -0,0 +1,31 @@
+pub struct Logger;
+
+impl core::fmt::Write for Logger {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        __kernel_println(s)
+    }
+}
+
+impl Logger {
+    pub const fn new() -> Self {
+        Self
+    }
+
+    pub fn write_fmt(&mut self, args: core::fmt::Arguments) -> core::fmt::Result {
+        core::fmt::Write::write_fmt(self, args)
+    }
+
+    pub fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        __kernel_println(s)
+    }
+
+    pub fn write_nl(&mut self) -> core::fmt::Result {
+        __kernel_println("\n")
+    }
+}
+
+#[doc(hidden)]
+pub fn __kernel_println(msg: &str) -> core::fmt::Result {
+    unsafe { ntapi::ntdbg::DbgPrint(msg.as_ptr() as _) };
+    Ok(())
+}
