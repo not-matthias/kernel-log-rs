38dc12392a1729fbca8f65202ef8467a65c09031
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-08-21 17:13:20 +0200

    Call `core::fmt::Write::write_str`

--- src/writer.rs
+++ src/writer.rs
@@ -18,7 +18,7 @@ impl KernelWriter {
     }
 
     pub fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        __kernel_println(s)
+        core::fmt::Write::write_str(self, s)
     }
 
     pub fn write_nl(&mut self) -> core::fmt::Result {
